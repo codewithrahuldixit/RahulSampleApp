02c69e112b09d43eb61aa9fa4860cfeed52dc2d5
rahul <posttorahuldixit@gmail.com>
2023-08-18 17:09:00 +0530

    Initial Commit Rahul Sample App

---
+++ src/main/java/com/example/RahulSampleApp/RahulSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.RahulSampleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahulSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahulSampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RahulSampleApp/RahulSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RahulSampleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RahulSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
